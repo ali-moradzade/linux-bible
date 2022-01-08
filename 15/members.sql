2fa2f109d2a5e01e2d7f59ba058af8fa47b36f45
Ali Moradzade <alimorizz1379@gmail.com>
2022-01-09 00:48:40 +0330

    adding chapter15: presenting data

---
+++ 15/members.sql
@@ -0,0 +1,8 @@
+INSERT INTO members (lname, fname, address, city, state, zip) VALUES
+	('Blum', 'Richard', '123 Main St.', 'Chicago', 'Chicago', '60601');
+INSERT INTO members (lname, fname, address, city, state, zip) VALUES
+	('Blum', 'Barbara', '123 Main St.', 'Chicago', 'Chicago', '60601');
+INSERT INTO members (lname, fname, address, city, state, zip) VALUES
+	('Bresnahan', 'Christine', '456 Oak Ave.', 'Columbus', 'Columbus', '43201');
+INSERT INTO members (lname, fname, address, city, state, zip) VALUES
+	('Bresnahan', 'Timothy', '456 Oak Ave.', 'Columbus', 'Columbus', '43201');
---
+++ 15/test5
@@ -0,0 +1,2 @@
+ls: cannot access 'test': No such file or directory
+ls: cannot access 'badtest': No such file or directory
